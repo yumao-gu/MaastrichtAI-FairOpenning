be2025e09317aca6c415cc966f94888c095bce7b
yumao-gu <yumao.gu@student.maastrichtuniversity.nl>
2020-06-11 20:33:47 +0200

    version2.0

--- fair.py
+++ fair.py
@@ -1,11 +1,12 @@
 from itertools import combinations,permutations
 import random
 
-positions = ['a','b','c','d','e']
+positions = ['a','b','c','d']
 players = ['A','B','C']
 low_prob = 0.
 players_number = 3
 test_num = 10
+rule = 2
 
 def State3():
     stateA = random.uniform(low_prob,1.)
@@ -51,7 +52,36 @@ def Project(player_id,position,states):
             project_states[key] = value
     return project_states
 
-def FindBest(player_id,states):#player_id 1,2,3
+def ReProject(id1,id2,state):
+    string = state['comb']
+    temp = string[id2]
+    trailer = string[id2+1:] if id2 + 1 < len(string) else ''
+    string = string[0:id2] + string[id1] + trailer
+    string = string[0:id1] + temp + string[id1+1:]
+    state['comb'] = string
+    state['prob'][id1],state['prob'][id2] = state['prob'][id2],state['prob'][id1]
+
+def ReOrder(id,state):
+    list1 = state["prob"][id:]
+    list2 = list(state["comb"][id:])
+    list1, list2 = (list(t) for t in zip(*sorted(zip(list1, list2))))
+    state["prob"][id:] = list1
+    state["comb"] = state["comb"][:id]+"".join(list2)
+
+def FindBest(rule,player_id,states):
+    assert player_id <= players_number
+    if player_id == players_number:
+        return FindBestCurrent(player_id,states)
+    elif rule == 0:
+        return FindBestCurrent(player_id,states)
+    elif rule == 1:
+        return FindBestToNext(player_id,states)
+    elif rule == 2:
+        return FindBestToEnd(player_id,states)
+    else:
+        print("Do not support the rule")
+
+def FindBestCurrent(player_id,states):#player_id 1,2,3
     best_state={}
     for key, value in states.items():
         best_state['comb']=key
@@ -60,6 +90,38 @@ def FindBest(player_id,states):#player_id 1,2,3
         if value[player_id-1] > best_state['prob'][player_id-1]:
             best_state['comb']=key
             best_state['prob']=value
+    return best_state
+
+def FindBestToNext(player_id,states):#player_id 1,2,3
+    assert player_id < players_number
+    best_state={}
+    for key, value in states.items():
+        best_state['comb']=key
+        best_state['prob']=value
+    for key, value in states.items():
+        # print(best_state)
+        if min(value[player_id-1],value[player_id]) \
+        >= min(best_state['prob'][player_id-1],best_state['prob'][player_id]):
+            best_state['comb']=key
+            best_state['prob']=value
+            # print(best_state)
+            if value[player_id-1] >= value[player_id]:
+                # print("ReProject")
+                ReProject(player_id-1,player_id,best_state)
+                # print(best_state)
+    assert best_state['prob'][player_id-1]<=best_state['prob'][player_id]
+    return best_state
+
+def FindBestToEnd(player_id,states):#player_id 1,2,3
+    best_state={}
+    for key, value in states.items():
+        best_state['comb']=key
+        best_state['prob']=value
+    for key, value in states.items():
+        if min(value[player_id-1:]) >= min(best_state['prob'][player_id-1:]):
+            best_state['comb']=key
+            best_state['prob']=value
+            ReOrder(player_id-1,best_state)
     return best_state
 
 def FindFair_d(states):
@@ -114,19 +176,19 @@ def Strategy3(states_space):
                 for positionC in positions:
                     if positionA is not positionC and positionB is not positionC :
                         projectC = Project(3,positionC,projectB)
-                        tmpC=FindBest(3,projectC)
-                        # print("tmpC\t{s}".format(s=tmpC))
+                        tmpC=FindBest(rule,3,projectC)
+                        print("tmpC\t{s}".format(s=tmpC))
                         bestCs[tmpC['comb']] = tmpC['prob']
-                # print("bestCs\t{s}".format(s=bestCs))
-                bestC = FindBest(3,bestCs)
-                # print("bestC\t{s}".format(s=bestC))
+                print("bestCs\t{s}".format(s=bestCs))
+                bestC = FindBest(rule,3,bestCs)
+                print("bestC\t{s}".format(s=bestC))
                 bestBs[bestC['comb']] = bestC['prob']
-        # print("bestBs\t{s}".format(s=bestBs))
-        bestB = FindBest(2,bestBs)
-        # print("bestB\t{s}".format(s=bestB))
+        print("bestBs\t{s}".format(s=bestBs))
+        bestB = FindBest(rule,2,bestBs)
+        print("bestB\t{s}".format(s=bestB))
         bestAs[bestB['comb']] = bestB['prob']
     print("bestAs\t{s}".format(s=bestAs))
-    return FindBest(1,bestAs)
+    return FindBest(rule,1,bestAs)
 
 def Strategy4(states_space):
     bestAs = {}
@@ -144,15 +206,15 @@ def Strategy4(states_space):
                         for positionD in positions:
                             if positionA is not positionD and positionB is not positionD and  positionC is not positionD:
                                 projectD = Project(4,positionD,projectC)
-                                tmpD=FindBest(4,projectD)
+                                tmpD=FindBest(rule,4,projectD)
                                 bestDs[tmpD['comb']] = tmpD['prob']
-                        bestD = FindBest(4,bestDs)
+                        bestD = FindBest(rule,4,bestDs)
                         bestCs[bestD['comb']] = bestD['prob']
-                bestC = FindBest(3,bestCs)
+                bestC = FindBest(rule,3,bestCs)
                 bestBs[bestC['comb']] = bestC['prob']
-        bestB = FindBest(2,bestBs)
+        bestB = FindBest(rule,2,bestBs)
         bestAs[bestB['comb']] = bestB['prob']
-    return FindBest(1,bestAs)
+    return FindBest(rule,1,bestAs)
 
 def Strategy(states_space,players_number):
     if players_number == 3:
@@ -166,7 +228,7 @@ if __name__ == "__main__":
     for i in range(test_num):
         print("---------------------%d------------------------" %(i))
         states_space = GenStates(positions,players_number)
-        # print(states_space)
+        print(states_space)
         fair_d = FindFair_d(states_space)
         fair_D = FindFair_D(states_space)
         fair_D_bar = FindFair_D_bar(states_space,players_number)
